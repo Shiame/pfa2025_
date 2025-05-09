88ef8678d2bfb8c66b0551154b6d38fa5dcf7b65
Bouazza Chaymae <108526847+Shiame@users.noreply.github.com>
2025-05-09 12:50:47 +0100

    Delete Backend directory

--- Backend/src/main/java/com/example/projet_PF2A/Repository/CategorieRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.projet_PF2A.Repository;
-
-import com.example.projet_PF2A.Model.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategorieRepo extends JpaRepository<Categorie, Long> {
-}
